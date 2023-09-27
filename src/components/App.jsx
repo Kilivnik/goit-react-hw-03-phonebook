b22adac0deb4221b33160fcd8f93414d1134fc3d
Kilivnik <ekstasy1990@gmail.com>
2023-09-27 12:21:39 +0300

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,6 +38,23 @@ class App extends Component {
         }));
   };
 
+  // Реализуэм сохранение контактов в localStorage
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts) ?? [];
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  // Реализуэм сохранение контактов в localStorage
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
     return (
